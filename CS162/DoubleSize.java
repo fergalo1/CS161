9e8616745ff7096e796b8c14400628f90f0aa906
fergalo1 <44031328+fergalo1@users.noreply.github.com>
2021-12-11 14:13:07 +0000

    Create DoubleSize.java

---
+++ CS162/DoubleSize.java
@@ -0,0 +1,22 @@
+public static void main(String args[])
+{
+  int a = 5;
+  int b = 4;
+  answer(a,b);
+}
+
+public static void answer(int a,int b)
+{
+ if(a < b)
+ {
+  System.out.println(b + " is greater than " + a); 
+ }
+  if(a > b)
+ {
+  System.out.println(a + " is greater than " + b); 
+ }
+  if(a == b)
+ {
+  System.out.println(b  + " is equal to " + a); 
+ }
+}
