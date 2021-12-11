f1b1ede9baa910feabda516df6c58474cc8f59bc
fergalo1 <44031328+fergalo1@users.noreply.github.com>
2021-12-11 14:09:38 +0000

    Create NestedForLoop.java

---
+++ CS161/Lab08/NestedForLoop.java
@@ -0,0 +1,15 @@
+public class NestedForLoop
+{
+    public static void main(String args[])
+    {
+        for(int i = 1;i<=12;i++)
+        {
+        for(int j = 0;j<=12;j++)
+        {
+            System.out.println(i +" * "+ j + " = " + (i*j));
+        }
+        System.out.println("*****");
+
+        }
+    }
+}
